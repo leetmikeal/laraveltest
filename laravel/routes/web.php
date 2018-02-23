35c2a931b26e0b584e7a84e3c6965de10ee6a591
Teppei Tamaki <tamaki@tmkmac.local>
2018-02-23 17:15:46 +0900

    added profile controller

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -18,3 +18,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/settings', function () {
+    return redirect('/settings/profile');
+})->name('settings');
+Route::get('/settings/profile', 'Settings\ProfileController@index')->name('settings_profile');
